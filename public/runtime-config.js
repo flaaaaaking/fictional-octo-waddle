940e9ff4adf5f81d221835d7685dfcff3634d96b
flaaaaaking <2629864558@qq.com>
2026-08-18 12:40:30 +0800

    Use same-origin temporary tunnel configuration

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,2 +1,2 @@
 // Public browser configuration only. Never add API keys, tokens, passwords or an origin IP here.
-window.__APP_CONFIG__ = { API_BASE_URL: 'https://intensive-brokers-answer-pregnant.trycloudflare.com' };
+window.__APP_CONFIG__ = { API_BASE_URL: '' };
